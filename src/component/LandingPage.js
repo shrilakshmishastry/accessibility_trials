7ee880e11671b19b4eba3bda6cdc13f40b846638
shrilakshmishastry <shrilakshmishastry@gmail.com>
2022-09-02 22:59:05 +0530

    add pagination

--- src/component/LandingPage.js
+++ src/component/LandingPage.js
@@ -1,8 +1,21 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Pagination from './Pagination';
 const LandingPage = () => {
   return (
-    <p>
-      Landing Page
-    </p>
+    <Container>
+      <Row className='mt-4 '>
+        <Col md={{ span: 12 }}>
+          <h2 className='fw-bold'>
+            The Trials
+          </h2>
+        </Col>
+      </Row>
+      <Row>
+        <Pagination />
+      </Row>
+    </Container>
   );
 }
 export default LandingPage;
